309f09bb76bc40b44075ba350cfe7be49ddc6e76
gongmiaomiao <gongmiaomiao@didiglobal.com>
2024-02-28 10:21:38 +0800

    最大矩形，最小栈

---
+++ src/main/java/com/leetcode/GlobalTest.java
@@ -0,0 +1,39 @@
+package com.leetcode;
+
+import com.leetcode.linked.ListNode;
+import com.leetcode.linked.ListNodeUtil;
+
+public class GlobalTest {
+    public static void main(String[] args) {
+        ListNode node5 = new ListNode(5, null);
+        ListNode node4 = new ListNode(4, node5);
+        ListNode node3 = new ListNode(3, node4);
+        ListNode node2 = new ListNode(2, node3);
+        ListNode node = new ListNode(1, node2);
+        ListNodeUtil.printAll(node);
+        ListNode reversed = reverse2(node);
+        ListNodeUtil.printAll(reversed);
+    }
+
+    public static ListNode reverse2(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode newHead = reverse2(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newHead;
+    }
+
+    public static ListNode reverse1(ListNode head) {
+        ListNode pre = null, next;
+        ListNode curr = head;
+        while (curr != null) {
+            next = curr.next;
+            curr.next = pre;
+            pre = curr;
+            curr = next;
+        }
+        return pre;
+    }
+}
--- src/main/java/com/leetcode/queue/MaxSlidingWindow.java
+++ src/main/java/com/leetcode/queue/MaxSlidingWindow.java
@@ -1,5 +1,10 @@
 package com.leetcode.queue;
 
+import com.leetcode.utils.Utils;
+
+import java.util.Arrays;
+import java.util.Map;
+
 /**
  * 239. 滑动窗口最大值
  * <a href="https://leetcode.cn/problems/sliding-window-maximum/description/">...</a>
@@ -11,19 +16,53 @@ package com.leetcode.queue;
  * 滑动窗口的位置                最大值
  * ---------------               -----
  * [1  3  -1] -3  5  3  6  7       3
- *  1 [3  -1  -3] 5  3  6  7       3
- *  1  3 [-1  -3  5] 3  6  7       5
- *  1  3  -1 [-3  5  3] 6  7       5
- *  1  3  -1  -3 [5  3  6] 7       6
- *  1  3  -1  -3  5 [3  6  7]      7
+ * 1 [3  -1  -3] 5  3  6  7       3
+ * 1  3 [-1  -3  5] 3  6  7       5
+ * 1  3  -1 [-3  5  3] 6  7       5
+ * 1  3  -1  -3 [5  3  6] 7       6
+ * 1  3  -1  -3  5 [3  6  7]      7
  */
 public class MaxSlidingWindow {
 
     public static void main(String[] args) {
-
+        int[] nums = {41, 8467, 6334, 6500, 9169, 572};
+        int k = 3;
+        int[] slidingWindow = maxSlidingWindow3(nums, k);
+        Utils.printArr(nums);
+        Utils.printArr(slidingWindow);
     }
 
-    public int[] maxSlidingWindow(int[] nums, int k) {
+    public static int[] maxSlidingWindow3(int[] nums, int k) {
         return nums;
+    }
+
+    // 暴力求解，改良版本
+    public static int[] maxSlidingWindow2(int[] nums, int k) {
+        if (nums.length < k) {
+            return nums;
+        }
+        int[] res = new int[nums.length - k + 1];
+        Arrays.fill(res, Integer.MIN_VALUE);
+        for (int i = 0; i <= nums.length - k; i++) {
+            for (int j = i; j < k + i; j++) {
+                res[i] = Math.max(res[i], nums[j]);
+            }
+        }
+        return res;
+    }
+
+    public static int[] maxSlidingWindow(int[] nums, int k) {
+        int[] res = new int[nums.length - k + 1];
+        int[] temp = new int[k];
+        for (int i = 0; i <= nums.length - k; i++) {
+            System.arraycopy(nums, i, temp, 0, k);
+            res[i] = maxArr(temp);
+        }
+        return res;
+    }
+
+    public static int maxArr(int[] nums) {
+        Arrays.sort(nums);
+        return nums[nums.length - 1];
     }
 }
