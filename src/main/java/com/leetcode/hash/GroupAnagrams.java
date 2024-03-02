360daef0d5b5b048d3e78bfbee4c4f148fb12a28
gongmiaomiao <gongmiaomiao@didiglobal.com>
2024-03-02 15:11:20 +0800

    字母异位词分组

--- src/main/java/com/leetcode/hash/GroupAnagrams.java
+++ src/main/java/com/leetcode/hash/GroupAnagrams.java
@@ -1,6 +1,6 @@
 package com.leetcode.hash;
 
-import java.util.List;
+import java.util.*;
 
 /**
  * 49. 字母异位词分组
@@ -12,10 +12,37 @@ import java.util.List;
  */
 public class GroupAnagrams {
     public static void main(String[] args) {
-
+        String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        List<List<String>> lists = groupAnagrams(strs);
+        System.out.println(lists);
     }
 
     public static List<List<String>> groupAnagrams(String[] strs) {
-        return null;
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            String sortStr = sortString(str);
+            List<String> list = map.getOrDefault(sortStr, new ArrayList<>());
+            list.add(str);
+            map.put(sortStr, list);
+        }
+        return new ArrayList<>(map.values());
+    }
+
+    public static List<List<String>> groupAnagrams1(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            String sortStr = sortString(str);
+            if (!map.containsKey(sortStr)) {
+                map.put(sortStr, new ArrayList<>());
+            }
+            map.get(sortStr).add(str);
+        }
+        return new ArrayList<>(map.values());
+    }
+
+    public static String sortString(String string) {
+        char[] chars = string.toCharArray();
+        Arrays.sort(chars);
+        return new String(chars);
     }
 }
